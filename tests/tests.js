e7f7dd4f84ee74d81c7ebf7503571988af242db7
Nimo Axmed <samatar_267@hotmail.com>
2017-06-19 01:50:48 +0100

    Add latest route test

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,19 @@
 const test = require('tape');
+const server = require('./../src/server.js');
 
 test('Test for Travis', t => {
   t.pass('Passing test');
   t.end();
+});
+
+test('/latest route', t => {
+  const options = {
+    url: '/latest',
+    method: 'GET',
+  };
+
+  server.inject(options, res => {
+    t.equal(res.statusCode, 200, 'statusCode should equal 200');
+    t.end();
+  });
 });
